035336d5c30f780e84ca23a9c529993041b3a0ec
ilkkj <ilkkajoa@gmail.com>
2024-11-21 22:03:44 +0200

    Add BibTeX function

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -7,3 +7,12 @@ class Reference:
 
     def __str__(self):
         return f"{self.title}, {self.author}, {self.year}"
+    
+    def bibtex(self):
+        key = "TESTI"
+        entry = f"""@book{{{key},
+    author = {{{self.author}}},
+    title = {{{self.title}}},
+    year = {{{self.year}}},
+}}"""
+        return entry
