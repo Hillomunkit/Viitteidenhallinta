03afa9c049906a7c53a96e88555f5652ae3ae1ca
Miki Kauste <miki.kauste@gmail.com>
2024-12-10 22:15:13 +0200

    Refactored the validations in util.py

--- src/util.py
+++ src/util.py
@@ -9,16 +9,26 @@ titles = {"book": "Teos",
         }
 
 def validate_reference(title, author, year, reference_type):
+    validate_title(title, reference_type)
+    validate_author(author)
+    validate_year(year)
+    return True
+
+def validate_title(title, reference_type):
     if not title or len(title.strip()) == 0:
         raise UserInputError(f"\"{titles[reference_type]}\" ei voi olla tyhjä")
     if len(title) > 100:
         raise UserInputError(f"\"{titles[reference_type]}\" maksimipituus on 100 merkkiä")
+    
+def validate_author(author):
     if not author or len(author.strip()) == 0:
         raise UserInputError("\"Kirjoittanut\" ei voi olla tyhjä")
     if len(author) > 100:
         raise UserInputError("\"Kirjoittanut\" maksimipituus on 100 merkkiä")
     if not re.match(r"^[a-zA-ZåäöÅÄÖ\s'-]+$", author):
         raise UserInputError("\"Kirjoittanut\" sisältää virheellisiä merkkejä")
+    
+def validate_year(year):
     current_year = datetime.now().year
     try:
         year_int = int(year)
@@ -26,5 +36,3 @@ def validate_reference(title, author, year, reference_type):
         raise UserInputError("\"Painovuosi\" tulee esittää numeroina")
     if year_int < 1000 or year_int > current_year:
         raise UserInputError(f"\"Painovuosi\" tulee olla 1000-{current_year} väliltä")
-
-    return True
